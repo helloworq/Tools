f4515398bf1a42081d56a27bccde5a0d1c997ff9
周磊 <1273370082@qq.com>
2020-12-23 11:04:03 +0800

    增加freemarker代码

--- src/main/java/com/zlutil/tools/ToolsApplication.java
+++ src/main/java/com/zlutil/tools/ToolsApplication.java
@@ -1,11 +1,13 @@
 package com.zlutil.tools;
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@NacosPropertySource(dataId = "example", autoRefreshed = true)
+@EnableSwagger2
 @SpringBootApplication
+@EntityScan("com.zlutil.tools.enity")
 public class ToolsApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/zlutil/tools/controller/GetData.java
+++ src/main/java/com/zlutil/tools/controller/GetData.java
@@ -6,6 +6,7 @@ import com.zlutil.tools.toolpackage.Feign.IndexValueQuery;
 import com.zlutil.tools.toolpackage.ResponseUtil.ResponseData;
 import com.zlutil.tools.toolpackage.ResponseUtil.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +16,17 @@ public class GetData {
     @Autowired
     ImsRestConfig imsRestConfig;
 
+    @Value("${spring.datasource.url}")
+    public String data;
+
     @RequestMapping("/getIndexValueQuery")
     public ResponseData getIndexValueQuery(){
         System.out.println(imsRestConfig.getIndexValueQuery());
         return ResponseUtil.success(imsRestConfig.getIndexValueQuery());
+    }
+
+    @RequestMapping("/getData")
+    public void getData(){
+        System.out.println(data);
     }
 }
